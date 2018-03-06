7685be4971e4ab0e18328f7869b91995db6093b7
EilishVanDerSnickt <32335854+EilishVanDerSnickt@users.noreply.github.com>
2018-03-06 11:03:36 +0100

    Add files via upload

--- Project MI2-Galgje/Galgje.js
+++ Project MI2-Galgje/Galgje.js
@@ -1,28 +1,42 @@
 "use strict";
 
-
-
-var getal = Math.floor(Math.random() * 5);
 var antwoord;
 
-
-switch (getal)
-    {
-        case 0: 
-           antwoord = "hallo";
-            break;
-        case 1: 
-            antwoord = "hey";
-            break;
-        case 2: 
-            antwoord = "hi";
-            break;
-        case 3: 
-            antwoord = "cava";
-            break;
-        case 4: 
-            antwoord = "goed";
-            break;
-            document.getElementById("galgje").innerHTML = antwoord;
+function RandomGenerator(antwoord){
+    var reactie;
+    var getal = Math.floor(Math.random() * 5);
+    
+    antwoord = reactie;
+    
+    switch (getal)
+        {
+            case 0: 
+                reactie = "hallo";
+                break;
+            case 1: 
+                reactie = "hey";
+                break;
+            case 2: 
+                reactie = "hi";
+                break;
+            case 3: 
+                reactie = "cava";
+                break;
+            case 4: 
+                reactie = "goed";
+                break;
     }
+    
+    document.getElementById("hetTeRadenWoord").innerHTML = reactie;
+   
+    return reactie;
+}
+
+RandomGenerator(antwoord);
+
+//document.getElementById("hetTeRadenWoord").innerHTML = antwoord;
+
+
+
+
 

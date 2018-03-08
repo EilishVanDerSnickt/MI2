f99b8f5c146cfe236982522fadc7137798fcbeda
EilishVanDerSnickt <32335854+EilishVanDerSnickt@users.noreply.github.com>
2018-03-08 12:54:46 +0100

    Add files via upload

---
+++ MI2/opdracht 13/opdracht13_JSON.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function verwerkJSON(response) {
+    var antwoord = "", i, adres;
+    
+    for (i = 0; i < response.results.length; i += 1)    
+    {      
+        adres = response.results[i];
+        
+        antwoord += "<tr><td>" + adres.address_components[0].long_name + "<\/td>";
+        antwoord += "<td>" + adres.address_components[1].long_name + "<\/td>";
+        antwoord += "<td>" + adres.address_components[2].long_name + "<\/td>";
+        antwoord += "<td>" + adres.adderess_components[3].long_name + "<\/td>";
+        antwoord += "<td>" + adres.geometry.location.lat + "<\/td>";
+        antwoord += "<td>" + adres.geometry.location.lng + "<\/td><\/tr>";
+    }
+    
+    $("#adres tbody").html(antwoord);
+}
+            
+$(function () {			
+    $.ajax
+    ({
+        type : "GET",
+        url : "http://maps.googleapis.com/maps/api/geocode/json?address=Bosstraat%20182,Lede,Belgie",
+        dataType : 'json',
+        success : verwerkJSON
+    });
+});
+
+			
+	

8f227adc99f1a471da49b0158e3d03a449dee748
EilishVanDerSnickt <32335854+EilishVanDerSnickt@users.noreply.github.com>
2018-03-29 09:06:33 +0200

    Add files via upload

---
+++ MI2/opdracht 15/opdracht15_JSON.js
@@ -0,0 +1,34 @@
+"use strict"
+
+$(document).ready(function () {
+
+function verwerkJSON(response) {
+    var antwoord = "", output, i, j, adres;
+    
+    for (i = 0; i < response.length; i += 1)
+    {
+        j = response[i].id;
+        output = response[i];
+        adres = "http://www.songsterr.com/a/wa/song?id=" + j;
+        antwoord += '<p><a href=' + adres + ' target="_blank">' + response[i].id + '. ' + response[i].artist.nameWithoutThePrefix +  ' - ' + response[i].title + '</a><p>';
+        console.log(response[i].title);
+            
+    }
+    $("#output").html(antwoord);
+}
+
+$("#zoek").on("click", function () {		
+    if (document.getElementById("artiest").value != "") {
+            $(function () {
+    $.ajax
+    ({
+        type : "GET",
+        url : "http://www.songsterr.com/a/ra/songs.json?pattern=" + $("#artiest").val(),
+        dataType : 'json',
+        success : verwerkJSON
+        })
+    })
+        
+}
+});
+});

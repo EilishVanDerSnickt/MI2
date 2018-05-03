fe5c1ef2aa349ae9ad4e8723c6ce233cba038944
EilishVanDerSnickt <32335854+EilishVanDerSnickt@users.noreply.github.com>
2018-05-03 12:35:30 +0200

    Add files via upload

---
+++ MI2/Opdracht 20/js/routes.js
@@ -0,0 +1,106 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+    name: 'home',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    name: 'about',
+  },
+  // Right Panel pages
+  {
+    path: '/panel-right-1/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 1</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 1</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+  {
+    path: '/panel-right-2/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 2</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 2</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+
+  // Components
+  {
+    path: '/voorstelling_persoon/',
+    url: './pages/voorstelling_persoon.html',
+  },
+     {
+    path: '/voorstelling_spel/',
+    url: './pages/voorstelling_spel.html',
+  },
+  // Color Themes
+  {
+    path: '/color-themes/',
+    componentUrl: './pages/color-themes.html',
+  },
+
+  // Page Loaders
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
+  },
+
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
